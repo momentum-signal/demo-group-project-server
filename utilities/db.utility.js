619e1cf1f5ed7ccac3ab0f301a66c136e187ad5d
hasibulislam999 <hasib143sl@gmail.com>
2022-10-07 07:06:18 +0600

    establishing DB done

---
+++ utilities/db.utility.js
@@ -0,0 +1,18 @@
+/* external import */
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+function dbConnection() {
+  mongoose
+    .connect(process.env.DB_LOCAL, { dbName: "demo-group-project" })
+    .then(() =>
+      console.log(
+        colors.yellow.bold("Success: establishing DB connection")
+      )
+    )
+    .catch((error) =>
+      console.log(colors.red.underline(`Error: ${error.name}`))
+    );
+}
+
+module.exports = dbConnection;
