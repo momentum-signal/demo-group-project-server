23051788e5f79bde8428bce944f8d8c5ed2204c9
hasibulislam999 <hasib143sl@gmail.com>
2022-10-09 02:05:34 +0600

    verify token middleware init

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,26 @@
+/* external imports */
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers?.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        acknowledgement: false,
+        message: "Unauthorized",
+        description:
+          "The request was a legal request, but the server is refusing to respond to it. For use when authentication is possible but has failed or not yet been provided",
+      });
+    }
+
+    const decoded = await promisify(jwt.verify)(
+      token,
+      process.env.TOKEN_SECRET
+    );
+    req.user = decoded;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
