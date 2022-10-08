abb7cc6de3ce6a451ad95973a965340292b00b55
hasibulislam999 <hasib143sl@gmail.com>
2022-10-09 01:33:41 +0600

    user service initialize

---
+++ services/user.service.js
@@ -0,0 +1,31 @@
+/* external import */
+const bcrypt = require("bcryptjs");
+
+/* internal import */
+const User = require("../schemas/user.schema");
+
+exports.registerAnUserService = async (data) => {
+  const user = new User(data);
+  const result = await user.save();
+  return result;
+};
+
+exports.loggedAnUserService = async (data) => {
+  const userInfo = await User.findOne({
+    email: data.email,
+  });
+  const { password, confirmPassword, ...user } = userInfo.toObject();
+
+  if (user) {
+    if (bcrypt.compareSync(data.password, password || confirmPassword)) {
+      return {
+        _redirect: true,
+        user,
+      };
+    } else {
+      return { _redirect: false, error: "Password is wrong!" };
+    }
+  } else {
+    return { _redirect: false, error: "User not exist!" };
+  }
+};
