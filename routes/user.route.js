dcae4d9b11c63f5b28218390d4efd06f004bdf46
hasibulislam999 <hasib143sl@gmail.com>
2022-10-09 01:49:55 +0600

    user route modified

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,11 +6,13 @@ const {
   registerAnUser,
   loggedAnUser,
   confirmEmail,
+  displayAllUsers,
 } = require("../controllers/user.controller");
 
 /* router level connection */
 const router = express.Router();
 
+router.get("/all", displayAllUsers);
 router.post("/signup", registerAnUser);
 router.post("/signin", loggedAnUser);
 router.get("/:token", confirmEmail);
