9eb33ec8e56add478d202dd4cfa39c3beba81cb2
hasibulislam999 <hasib143sl@gmail.com>
2022-10-09 08:52:51 +0600

    jwt token authentication done

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,11 +8,12 @@ const {
   confirmEmail,
   displayAllUsers,
 } = require("../controllers/user.controller");
+const verifyToken = require("../middlewares/verifyToken.middleware");
 
 /* router level connection */
 const router = express.Router();
 
-router.get("/all", displayAllUsers);
+router.get("/all", verifyToken, displayAllUsers);
 router.post("/signup", registerAnUser);
 router.post("/signin", loggedAnUser);
 router.get("/:token", confirmEmail);
