04fa1e5e63370efaddddca305acf94f16d2d66e3
hasibulislam999 <hasib143sl@gmail.com>
2022-10-09 12:13:56 +0600

    db utility updated

--- utilities/db.utility.js
+++ utilities/db.utility.js
@@ -4,11 +4,14 @@ const colors = require("colors");
 
 function dbConnection() {
   mongoose
-    .connect(process.env.DB_URI, { dbName: "demo-group-project" })
+    .connect(process.env.DB_URI, {
+      dbName: "demo-group-project",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
     .then(() =>
-      console.log(
-        colors.yellow.bold("Success: establishing DB connection")
-      )
+      console.log(colors.yellow.bold("Success: establishing DB connection"))
     )
     .catch((error) =>
       console.log(colors.red.underline(`Error: ${error.name}`))
