83df06e5a268267aa28a1b780121535f02dadf34
hasibulislam999 <hasib143sl@gmail.com>
2022-10-09 11:01:33 +0600

    user route modified

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,6 +7,7 @@ const {
   loggedAnUser,
   confirmEmail,
   displayAllUsers,
+  resetPassword,
 } = require("../controllers/user.controller");
 const verifyToken = require("../middlewares/verifyToken.middleware");
 
@@ -17,5 +18,6 @@ router.get("/all", verifyToken, displayAllUsers);
 router.post("/signup", registerAnUser);
 router.post("/signin", loggedAnUser);
 router.get("/:token", confirmEmail);
+router.patch("/reset", resetPassword);
 
 module.exports = router;
