2bf8b6740e9d89b1368fbc5065ff5c8df9e64982
hasibulislam999 <hasib143sl@gmail.com>
2022-10-09 13:02:52 +0600

    driver modified

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 const express = require("express");
 const cors = require("cors");
 const colors = require("colors");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 /* internal imports */
@@ -23,7 +24,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 /* middleware connection */
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static("avatars"));
 
